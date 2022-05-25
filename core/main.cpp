0b80c31f37318713f8fa0d10f95ac9289741fc5e
TheNuclearDart <66973047+TheNuclearDart@users.noreply.github.com>
2022-05-25 17:00:25 -0600

    Got printf working, added some basic prints

--- core/main.cpp
+++ core/main.cpp
@@ -303,15 +303,14 @@ int main(void)
    MX_GPIO_Init(); // This needs replacing or fixing, wasn't needed for UART like thought
 
    print.init();
+   printf("Print initialized!\r\n");
    //usb.start(); // Start USB host. Was initialized at declaration
 
+   printf("Initialization complete, beginning main loop\r\n");
+   uint32_t i = 0;
    while (1)
    {
-      // Going to replace this whole idea with just a printf redirect, which is better. Probably reuse the class as something to wrap the hw for printf
-      uint8_t data[] = "This is a test using print.out!!\n\r";
-      //print.outRaw(data, sizeof(data), 10);
-      print.out(data[0], sizeof(data)); // I don't think this is ideal
-      printf("This is a printf test\n\r");
+      printf("This is a printf test. i = %ld\r\n", i++);
       HAL_Delay(1000);
 
       //usb.process();
