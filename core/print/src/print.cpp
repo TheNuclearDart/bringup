6a5de7953b3ce37a15ea75625f14f190302366b8
TheNuclearDart <66973047+TheNuclearDart@users.noreply.github.com>
2022-06-09 16:38:46 -0600

    Add init guard to print library

--- core/print/src/print.cpp
+++ core/print/src/print.cpp
@@ -1,4 +1,8 @@
+#include <stdio.h>
+
 #include "print.h"
+
+bool initialized = false;
 
 /**
  * @brief Simple wrapper for setting up UART to print. Not sure this is the best way to do this
@@ -13,26 +17,36 @@ Print::Print(UART_HandleTypeDef &uart, uint32_t timeout)
 
 Print::Print(USART_TypeDef *uartInstance, uint32_t baudRate, uint32_t wordLength, uint32_t stopBits, uint32_t parity, uint32_t mode, uint32_t hwFlowCtl, uint32_t overSampling, uint32_t oneBitSampling, uint32_t timeout)
 {
-   this->uartInstance = 
+   if (initialized)
    {
-      .Instance = uartInstance,
-      .Init =
+      printf("Print library already initialized...\r\n");
+      return;
+   }
+   else
+   {
+      initialized = true;
+   
+      this->uartInstance = 
       {
-         .BaudRate = baudRate,
-         .WordLength = wordLength,
-         .StopBits = stopBits,
-         .Parity = parity,
-         .Mode = mode,
-         .HwFlowCtl = hwFlowCtl,
-         .OverSampling = overSampling,
-         .OneBitSampling = oneBitSampling
-      },
-      .AdvancedInit =
-      {
-         .AdvFeatureInit = UART_ADVFEATURE_NO_INIT
-      }
-   };
-   this->timeout = timeout;
+         .Instance = uartInstance,
+         .Init =
+         {
+            .BaudRate = baudRate,
+            .WordLength = wordLength,
+            .StopBits = stopBits,
+            .Parity = parity,
+            .Mode = mode,
+            .HwFlowCtl = hwFlowCtl,
+            .OverSampling = overSampling,
+            .OneBitSampling = oneBitSampling
+         },
+         .AdvancedInit =
+         {
+            .AdvFeatureInit = UART_ADVFEATURE_NO_INIT
+         }
+      };
+      this->timeout = timeout;
+   }
 }
 
 void Print::init(void)
@@ -49,4 +63,9 @@ void Print::out(const uint8_t &data, const uint32_t size)
 void Print::outRaw(const uint8_t *pData, uint16_t Size, uint32_t Timeout)
 {
    HAL_UART_Transmit(&this->uartInstance, pData, Size, Timeout);
+}
+
+bool print_is_initialized(void)
+{
+   return initialized;
 }
