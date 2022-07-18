916403065f4bd2bf4698d9516a8044fd8eed3137
TheNuclearDart <66973047+TheNuclearDart@users.noreply.github.com>
2022-07-18 13:42:06 -0600

    Update BL to be 64KB, app images to 448KB
    Hopefully weird size doesn't cause issues

--- core/memory_layout/inc/memory_layout.h
+++ core/memory_layout/inc/memory_layout.h
@@ -4,6 +4,7 @@ extern uint32_t __app_start__;
 extern uint32_t __app_size__;
 extern uint32_t __sram_start__;
 
-const uint32_t fw_image_size = (reinterpret_cast<uint32_t>(&__app_size__)) / 2;
-const uint32_t *fw_image_A_ptr = &__app_start__;
-const uint32_t *fw_image_B_ptr = fw_image_A_ptr + fw_image_size;
+constexpr uint32_t fw_image_size = 0x70000; // 256KB for now, not sure that this will work forever. Currently only have 240k reserved in memory map (as that is max SRAM size)
+                                            // This actually doesn't work, as the bootloader occupies the first sector of flash.  
+constexpr uint32_t *fw_image_A_ptr = &__app_start__;
+constexpr uint32_t *fw_image_B_ptr = fw_image_A_ptr + fw_image_size;
