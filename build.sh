40d4e7ed7fe99eb52ed7b02c5a70b3c8030af28e
TheNuclearDart <66973047+TheNuclearDart@users.noreply.github.com>
2022-07-22 15:03:25 -0600

    Update build.sh to use build_image.py only

--- build.sh
+++ build.sh
@@ -9,9 +9,8 @@ else
    cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../arm-none-eabi-gcc.cmake ../ -Wno-dev
    make -j 8
    hash=$(git rev-parse --short HEAD)
-   python ../build_image.py --elf-path ./blink_stripped.elf --sections-path ./sections.txt
+   python ../build_image.py --hash 0x$hash --elf-path ./blink_stripped.elf --sections-path ./sections.txt
    # Concatenate the .bins into one larger binary.
-   python ../write_header.py --hash 0x$hash --bin-path ./image.bin
    cat ./core/bootloader/bootloader.bin ./image.bin > full_image.bin
    echo "Full image built at ./build/full_image.bin"
 fi
