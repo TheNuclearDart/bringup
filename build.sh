a2d8e3a0abeacb6844bd3246fda9da723d1efe4b
TheNuclearDart <66973047+TheNuclearDart@users.noreply.github.com>
2022-07-28 09:27:27 -0600

    Fix python usage
    Accidentally used Python2.7 instance which was removed in MacOS update
    
    Now using Python3, fixed build_image.py for this

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ else
    cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../arm-none-eabi-gcc.cmake ../ -Wno-dev
    make -j 8
    hash=$(git rev-parse --short HEAD)
-   python ../build_image.py --hash 0x$hash --elf-path ./blink_stripped.elf --sections-path ./sections.txt
+   python3 ../build_image.py --hash 0x$hash --elf-path ./blink_stripped.elf --sections-path ./sections.txt
    # Concatenate the .bins into one larger binary.
    cat ./core/bootloader/bootloader.bin ./image.bin > full_image.bin
    echo "Full image built at ./build/full_image.bin"
